c7ac3626481238e607782f81848e6874c9702fe6
Daniel Ibay <danielibay1538ooutlook.com>
2025-03-20 23:29:43 +0100

    Arreglar error de build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    resolve: {
+      // Fuerza a Vite a resolver las rutas sin inyectar bytes nulos
+      preserveSymlinks: true,
+    },
+    // Deshabilita la caché de optimización de dependencias
+    cacheDir: undefined,
+    plugins: [tailwindcss()],
+  },
 });
